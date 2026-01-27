12c7cb8a07cefd84d3e11ca435447c69890b72e2
andres <desarrollolibre.net@gmail.com>
2026-01-27 05:49:48 -0400

    EJ disconnect y limimante de ws actual

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Request, WebSocket
+from fastapi import FastAPI, APIRouter, Request, WebSocket, WebSocketDisconnect
 
 from fastapi.templating import Jinja2Templates
 
@@ -18,7 +18,20 @@ def form(request: Request):
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        print('Hola')
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    try:
+        while True:
+            print('conection open')
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        # --- EVENTO: AL DESCONECTARSE (Cierre limpio) ---
+        print("El cliente cerró la conexión")
+        
+    except Exception as e:
+        # Captura otros errores inesperados
+        print(f"Error inesperado: {e}")
+        
+    finally:
+        # --- LÓGICA FINAL ---
+        # Este bloque se ejecuta siempre, ideal para limpieza de recursos
+        print("Limpieza de conexión finalizada")
---
+++ api_multiple.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, APIRouter, Request, WebSocket, WebSocketDisconnect
+
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates/")
+
+
+# uvicorn api:app --reload
+# 
+
+app = FastAPI()
+router = APIRouter()
+
+@app.get('/')
+def form(request: Request):
+    return templates.TemplateResponse(request=request, name='ws/chat.html')
+
+from typing import List
+
+class ConnectionManager:
+    def __init__(self):
+        # Lista para almacenar las conexiones activas
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Cliente #{client_id} dice: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Cliente #{client_id} se ha desconectado")
